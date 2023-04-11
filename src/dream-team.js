af9cf13ee0f54408a04458008b02b52be63067eb
Vladimir Arshinnikov <lordofrain7@gmail.com>
2023-04-11 16:45:43 +0300

    sum-degetes comlete

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,29 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arrValueNames) {
+  if (Array.isArray(arrValueNames)) {
+    let secretTeamName = [];
+    for (let i = 0; i < arrValueNames.length; i++) {
+      if (typeof arrValueNames[i] === 'string') {
+        secretTeamName.push(arrValueNames[i].trim().charAt(0).toUpperCase());
+      }
+    }
+    return secretTeamName.sort().join('');
+  } else return false;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,11 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(num) {
+  if (num <= 9) {
+    return num;
+  }
+
+  return getSumOfDigits(
+    String(num)
+      .split('')
+      .reduce((acc, digit) => acc + Number(digit), 0)
+  );
 }
+// remove line with error and write your code here
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
